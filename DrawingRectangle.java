86fd9bf42a770055d2f7951d34c9bdb7828ace29
Jade Pearl <114611334+jadepearl1@users.noreply.github.com>
2023-08-02 10:54:45 -0400

    Create DrawingRectangle.java

---
+++ DrawingRectangle.java
@@ -0,0 +1,13 @@
+public class DrawingRectangle {
+  public static void main(String[] args){
+    // TODO Auto-generated method stub
+    System.out.println("**********");
+    System.out.println("**********");
+    System.out.println("**********");
+  }
+}
+
+\* Output:
+**********
+**********
+********** */
